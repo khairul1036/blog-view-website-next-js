dcd135a3b5a14cb0d362d9892e7b7e9a53aa282f
Khairul Islam <khairul15-4102@diu.edu.bd>
2025-01-23 05:46:50 +0600

    change profile info

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,12 +5,14 @@ const Profile = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
+  console.log(user);
+
   if (!user) {
     // Redirect unauthenticated users to login
     return redirect("/api/auth/login");
   }
 
-  return <div>Welcome to your profile, {user.name}!</div>; // Show user's name or info if available
+  return <div>Welcome to your profile, {user.given_name}!</div>; // Show user's name or info if available
 };
 
 export default Profile;
